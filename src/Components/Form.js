838c266677578424c9c388f3d46684462c624b53
mini-militant <shailesh.coolcalm007@gmail.com>
2019-10-13 19:21:58 +0530

    [Shailesh]

---
+++ src/Components/Form.js
@@ -0,0 +1,87 @@
+import React from "react";
+import MenuList from '@material-ui/core/MenuList';
+import MenuItem from '@material-ui/core/MenuItem';
+
+
+
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loanAmount: 500,
+      loanDuration: 6
+    };
+  }
+  handleLoanAmountChange = event => {
+    this.setState({ loanAmount: event.target.value });
+  };
+
+  handleLoanDurationChange = event => {
+    this.setState({ loanDuration: event.target.value });
+  };
+
+  render() {
+    return (
+      <div>
+
+          <div className="sidebar">
+            <MenuList>
+              {Object.values(
+                JSON.parse(localStorage.getItem("localStore")) || []
+              ).map(item => (
+                <MenuItem
+                  onClick={() =>
+                    this.setState({
+                      loanAmount: item.loanAmount,
+                      loanDuration: item.loanDuration
+                    })
+                  }
+                >
+                  LoanAmount : {item.loanAmount} <br />
+                  LoanDuration : {item.loanDuration}
+                </MenuItem>
+              ))}
+            </MenuList>
+            </div>
+
+            <form onSubmit={this.props.displayMonthlyPay}>
+              <label className="label">
+                Loan Amount :
+                <input
+                  type="text"
+                  name="loanAmount"
+                  value={this.state.loanAmount}
+                  onChange={this.handleLoanAmountChange}
+                />
+                <div className="slidecontainer">
+                  <input
+                    type="range"
+                    min="500"
+                    max="5000"
+                    value={this.state.loanAmount}
+                    className="slider"
+                    id="myRange"
+                    onChange={this.handleLoanAmountChange}
+                  />
+                </div>
+              </label>
+              <br />
+              <label className="label">
+                Loan Duration :
+                <input
+                  type="text"
+                  name="loanDuration"
+                  value={this.state.loanDuration}
+                  onChange={this.handleLoanDurationChange}
+                />
+              </label>
+              <br />
+              <button>Check</button>
+            </form>
+            </div>
+
+
+    );
+  }
+}
+export default Form;
---
+++ src/Components/LoanCalculator.js
@@ -0,0 +1,68 @@
+import React from "react";
+import Form from "./Form";
+
+import "../css/Form.css";
+
+class LoanCalculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      interestRate: "",
+      monthlyPay: "",
+      displayMonthly: false
+    };
+    this.localStore = [];
+  }
+  handleChange = event => {
+    this.setState({ loanAmount: event.target.value });
+  };
+
+  displayMonthlyPay = async event => {
+    event.preventDefault();
+    console.log(this.state.loanDuration);
+    const loanAmount = event.target.elements.loanAmount.value;
+    const loanDuration = event.target.elements.loanDuration.value;
+    const api_call = await fetch(
+      `https://ftl-frontend-test.herokuapp.com/interest?amount=${loanAmount}&numMonths=${loanDuration}`
+    );
+    const data = await api_call.json();
+    this.setState({
+      displayMonthly: true,
+      interestRate: data.interestRate,
+      monthlyPay: data.monthlyPayment.amount
+    });
+
+    this.localStore.push({
+      loanAmount: loanAmount,
+      loanDuration: loanDuration
+    });
+    localStorage.setItem("localStore", JSON.stringify(this.localStore));
+  };
+
+  render() {
+
+
+    return (
+      <div className="wrapper">
+        <div className="main">
+
+          <div className="form-container">
+            <Form displayMonthlyPay={this.displayMonthlyPay} />
+            {this.state.displayMonthly ? (
+              <div className="label" >
+                <br />
+                <p>Interest Rate:{this.state.interestRate}</p>
+                <br />
+                <p>Monthly Pay:{this.state.monthlyPay}</p>
+              </div>
+            ) : null}
+            </div>
+
+            </div>
+          </div>
+
+    );
+  }
+}
+
+export default LoanCalculator;
